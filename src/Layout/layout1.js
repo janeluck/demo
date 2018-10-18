cd1f34db4274efb30b5409485ec894f69aaf0f0b
janeluck <janeluck158@gmail.com>
2018-10-18 10:44:01 +0800

    ok

--- src/Layout/layout1.js
+++ src/Layout/layout1.js
@@ -10,32 +10,42 @@ export default class Layout1 extends React.PureComponent {
         <div className="div1">
           <img src={image} alt="unnatural" className="img" />
           <p>
-          The lemon, Citrus limon (L.) Osbeck, is a species of small evergreen tree in the flowering plant family Rutaceae, native to South Asia, primarily North eastern
+            The lemon, Citrus limon (L.) Osbeck, is a species of small evergreen
+            tree in the flowering plant family Rutaceae, native to South Asia,
+            primarily North eastern
           </p>
         </div>
 
         <div className="div2">
-        <img src={image} alt="unnatural" className="img" />
+          <img src={image} alt="unnatural" className="img" />
           <p>
-          The lemon is a species of small evergreen tree in the flowering plant family Rutaceae
+            The lemon is a species of small evergreen tree in the flowering
+            plant family Rutaceae
           </p>
         </div>
-
-
         <p>浮动实现的“文字环绕图片效果”，未清除浮动：</p>
-          <div >
-
-        <img src={image} alt="unnatural" className="img" />
+        <div>
+          <img src={image} alt="unnatural" className="img" />
           <p>
-          The lemon is a species of small evergreen tree in the flowering plant family Rutaceae
-          The lemon, Citrus limon (L.) Osbeck, is a species of small evergreen tree in the flowering plant family Rutaceae, native to South Asia, primarily North eastern
-          The lemon, Citrus limon (L.) Osbeck, is a species of small evergreen tree in the flowering plant family Rutaceae, native to South Asia, primarily North eastern
-          The lemon, Citrus limon (L.) Osbeck, is a species of small evergreen tree in the flowering plant family Rutaceae, native to South Asia, primarily North eastern
-          The lemon, Citrus limon (L.) Osbeck, is a species of small evergreen tree in the flowering plant family Rutaceae, native to South Asia, primarily North eastern
-          The lemon, Citrus limon (L.) Osbeck, is a species of small evergreen tree in the flowering plant family Rutaceae, native to South Asia, primarily North eastern
-          The lemon, Citrus limon (L.) Osbeck, is a species of small evergreen tree in the flowering plant family Rutaceae, native to South Asia, primarily North eastern
-          The lemon, Citrus limon (L.) Osbeck, is a species of small evergreen tree in the flowering plant family Rutaceae, native to South Asia, primarily North eastern
-
+            The lemon is a species of small evergreen tree in the flowering
+            plant family Rutaceae The lemon, Citrus limon (L.) Osbeck, is a
+            species of small evergreen tree in the flowering plant family
+            Rutaceae, native to South Asia, primarily North eastern The lemon,
+            Citrus limon (L.) Osbeck, is a species of small evergreen tree in
+            the flowering plant family Rutaceae, native to South Asia, primarily
+            North eastern The lemon, Citrus limon (L.) Osbeck, is a species of
+            small evergreen tree in the flowering plant family Rutaceae, native
+            to South Asia, primarily North eastern The lemon, Citrus limon (L.)
+            Osbeck, is a species of small evergreen tree in the flowering plant
+            family Rutaceae, native to South Asia, primarily North eastern The
+            lemon, Citrus limon (L.) Osbeck, is a species of small evergreen
+            tree in the flowering plant family Rutaceae, native to South Asia,
+            primarily North eastern The lemon, Citrus limon (L.) Osbeck, is a
+            species of small evergreen tree in the flowering plant family
+            Rutaceae, native to South Asia, primarily North eastern The lemon,
+            Citrus limon (L.) Osbeck, is a species of small evergreen tree in
+            the flowering plant family Rutaceae, native to South Asia, primarily
+            North eastern
           </p>
         </div>
       </div>
