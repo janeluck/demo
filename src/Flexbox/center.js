a3ee65e0f15791f3e658aec675e1bd9ed68a8446
janeluck <janeluck158@gmail.com>
2018-11-22 21:05:54 +0800

    flexbox

--- src/Flexbox/center.js
+++ src/Flexbox/center.js
@@ -5,9 +5,9 @@ import "./center.css";
 export default  class FlexboxCenter extends React.Component {
     render() {
       return (
-        <div className="demo-flexbox-parent">
-          <div className="demo-flexbox-conten">1</div>
-          <div className="demo-flexbox-conten">2</div>
+        <div className="demo-flexbox-center-parent">
+          <div className="demo-flexbox-center-content">1</div>
+          <div className="demo-flexbox-center-content">2</div>
         </div>
       );
     }
